82834d065d4959363b62f2be553f575032e667ba
Boris Dušek <dusek@brailcom.org>
2011-05-28 17:29:54 +0200

    Fix gconf_list fix (forgotten comma).

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ define gconf_value($value, $type="string", $source="/etc/gconf/gconf.xml.default
 define gconf_list($value, $ltype="string", $source="/etc/gconf/gconf.xml.defaults") {
 	exec {"set $name to $value":
 		command => "gconftool-2 --direct --config-source=xml:readwrite:/etc/gconf/gconf.xml.defaults -s --type=list --list-type=$ltype $name [$value]",
-		path => "/usr/bin"
+		path => "/usr/bin",
 		onlyif => "test \"[$value]\" != \"`gconftool-2 --direct --config-source=xml:readwrite:/etc/gconf/gconf.xml.defaults -g $name`\"",
 	}
 }
